4b39231dde0932c6e9498502dbddfabb9e37708e
wwan13 <wwan13@naver.com>
2023-11-09 23:26:33 +0900

    feat : 공통 에러 메세지 선언

---
+++ src/main/java/christmas/domain/constant/ErrorMessage.java
@@ -0,0 +1,10 @@
+package christmas.domain.constant;
+
+public class ErrorMessage {
+
+    public static final String INVALID_DATE = "유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    public static final String INVALID_ORDER = "유효하지 않은 주문입니다. 다시 입력해 주세요.";
+
+    private ErrorMessage() {
+    }
+}
