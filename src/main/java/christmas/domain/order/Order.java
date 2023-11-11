35c7bb9bb5d1b239f7a757a6f72a2b64367beccf
wwan13 <wwan13@naver.com>
2023-11-12 02:39:11 +0900

    feat : `Order` 기능 구현 - 메뉴와 수량을 가진 문자열 리스트로 생성환다. - 총 주문 금액을 계산할 수 있다. - 주문에 입력한 메뉴 타입이 몇 개 있는지 알 수 있다. - [예외] 총 주문 금액이 10000원 미만인 경우 예외가 발생한다. - [예외] 주문 개수가 20을 초과하는 경우 예외가 발생한다. - [예외] 주문 메뉴 중 음료만 있는 경우 예외가 발생한다. - [예외] 메뉴의 주문 개수가 1 미만인 경우 예외가 발생하한다. - [예외] 중복 메뉴를 입력한 경우 예외가 발생한다. - [예외] 존재하지 않는 메뉴를 입력한 경우 예외가 발생한다.

---
+++ src/main/java/christmas/domain/order/Order.java
@@ -0,0 +1,118 @@
+package christmas.domain.order;
+
+import static christmas.domain.constant.Constant.ORDER_MAXIMUM_AMOUNT;
+import static christmas.domain.constant.Constant.ORDER_MINIMUM_AMOUNT;
+import static christmas.domain.constant.Constant.ORDER_MINIMUM_PRICE;
+import static christmas.domain.constant.ErrorMessage.INVALID_ORDER;
+import static christmas.domain.menu.MenuType.DRINK;
+
+import christmas.domain.menu.Menu;
+import christmas.domain.menu.MenuType;
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
+public class Order {
+
+    private static final String DELIMITER = "-";
+    private static final int MENU_INDEX = 0;
+    private static final int AMOUNT_INDEX = 1;
+
+    private final Map<Menu, Integer> order;
+
+    private Order(Map<Menu, Integer> order) {
+        this.order = order;
+    }
+
+    public static Order of(List<String> menuAmounts) {
+        Map<Menu, Integer> order = createOrder(menuAmounts);
+        validateMinimumOrderPrice(order);
+        validateMaximumOrderAmount(order);
+        validateOnlyDrink(order);
+        return new Order(order);
+    }
+
+    private static void validateMinimumOrderPrice(Map<Menu, Integer> order) {
+        int totalPrice = order.keySet().stream()
+            .mapToInt(menu -> menu.getPrice() * order.get(menu))
+            .sum();
+
+        if (totalPrice < ORDER_MINIMUM_PRICE) {
+            throw new IllegalArgumentException(INVALID_ORDER);
+        }
+    }
+
+    private static void validateMaximumOrderAmount(Map<Menu, Integer> order) {
+        int totalAmount = order.keySet().stream()
+            .mapToInt(order::get)
+            .sum();
+
+        if (totalAmount > ORDER_MAXIMUM_AMOUNT) {
+            throw new IllegalArgumentException(INVALID_ORDER);
+        }
+    }
+
+    private static void validateOnlyDrink(Map<Menu, Integer> order) {
+        boolean isOnlyDrink = order.keySet().stream()
+            .map(Menu::getType)
+            .allMatch(menuType -> menuType.equals(DRINK));
+
+        if (isOnlyDrink) {
+            throw new IllegalArgumentException(INVALID_ORDER);
+        }
+    }
+
+    private static Map<Menu, Integer> createOrder(List<String> menuAmounts) {
+        Map<Menu, Integer> order = new EnumMap<>(Menu.class);
+
+        menuAmounts.forEach(menuAmount -> {
+            Menu menu = Menu.of(menuAmount.split(DELIMITER)[MENU_INDEX]);
+            validateMenuContainsInOrder(menu, order);
+
+            int amount = parseIntWithExceptionThrow(menuAmount.split(DELIMITER)[AMOUNT_INDEX]);
+            validateAmountPerMenu(amount);
+
+            order.put(menu, amount);
+        });
+
+        return order;
+    }
+
+    private static void validateMenuContainsInOrder(Menu menu, Map<Menu, Integer> order) {
+        if (order.keySet().contains(menu)) {
+            throw new IllegalArgumentException(INVALID_ORDER);
+        }
+    }
+
+    private static int parseIntWithExceptionThrow(String value) {
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_ORDER);
+        }
+    }
+
+    private static void validateAmountPerMenu(int amount) {
+        if (amount < ORDER_MINIMUM_AMOUNT) {
+            throw new IllegalArgumentException(INVALID_ORDER);
+        }
+    }
+
+    public int calculateTotalPrice() {
+        return order.keySet().stream()
+            .mapToInt(money -> money.getPrice() * order.get(money))
+            .sum();
+    }
+
+    public int countMenuTypeAmount(MenuType menuType) {
+        return order.keySet().stream()
+            .filter(menu -> menu.getType().equals(menuType))
+            .mapToInt(order::get)
+            .sum();
+    }
+
+    public Map<Menu, Integer> getOrder() {
+        return Collections.unmodifiableMap(order);
+    }
+}
