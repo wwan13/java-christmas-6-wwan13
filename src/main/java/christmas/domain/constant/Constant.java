3a96d25a54ae78b7d441d1ecdd9432bdb9b99ff1
wwan13 <wwan13@naver.com>
2023-11-12 02:39:25 +0900

    feat : `Order` 관련 상수 추가

---
+++ src/main/java/christmas/domain/constant/Constant.java
@@ -0,0 +1,11 @@
+package christmas.domain.constant;
+
+public class Constant {
+
+    public static final int ORDER_MINIMUM_PRICE = 10_000;
+    public static final int ORDER_MINIMUM_AMOUNT = 1;
+    public static final int ORDER_MAXIMUM_AMOUNT = 20;
+
+    private Constant() {
+    }
+}
