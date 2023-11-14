f78e00236fc01c20853da2da43554ef1aeaf6e48
wwan13 <wwan13@naver.com>
2023-11-14 23:30:56 +0900

    fix : `Discount` 수정 - 공통 검증 로직 default 메소드로 통합 - isApplicableCore() 시그니처 변경

--- src/main/java/christmas/domain/event/discount/Discount.java
+++ src/main/java/christmas/domain/event/discount/Discount.java
@@ -1,11 +1,21 @@
 package christmas.domain.event.discount;
 
+import static christmas.domain.constant.Constant.ORDER_MINIMUM_PRICE;
+
 import christmas.domain.date.Date;
 import christmas.domain.order.Order;
 
 public interface Discount {
 
-    boolean isApplicable(Date date);
+    default boolean isApplicable(Date date, Order order) {
+        if (order.calculateTotalPrice() < ORDER_MINIMUM_PRICE) {
+            return false;
+        }
+
+        return isApplicableCore(date, order);
+    }
+
+    boolean isApplicableCore(Date date, Order order);
 
     int offer(Date date, Order order);
 }
