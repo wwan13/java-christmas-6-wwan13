76da29eed88e3495005f031ca6bcfdd534fa9767
wwan13 <wwan13@naver.com>
2023-11-15 03:03:57 +0900

    feat : PresentEvent 관련 상수 추가, 정수 형식 변경

--- src/main/java/christmas/domain/constant/Constant.java
+++ src/main/java/christmas/domain/constant/Constant.java
@@ -1,6 +1,9 @@
 package christmas.domain.constant;
 
+import static christmas.domain.menu.Menu.CHAMPAGNE;
+
 import christmas.domain.date.Date;
+import christmas.domain.menu.Menu;
 import java.util.List;
 
 public class Constant {
@@ -18,15 +21,19 @@ public class Constant {
 
     public static final Date CHRISTMAS_DAY_EVENT_START_DATE = Date.valueOf(1);
     public static final Date CHRISTMAS_DAY_EVENT_END_DATE = Date.valueOf(25);
-    public static final int CHRISTMAS_DAY_EVENT_DEFAULT_DISCOUNT_AMOUNT = 1000;
+    public static final int CHRISTMAS_DAY_EVENT_DEFAULT_DISCOUNT_AMOUNT = 1_000;
     public static final int CHRISTMAS_DAY_EVENT_ADDITIONAL_DISCOUNT_AMOUNT = 100;
 
-    public static final int WEEK_EVENT_DISCOUNT_AMOUNT = 2023;
+    public static final int WEEK_EVENT_DISCOUNT_AMOUNT = 2_023;
 
     public static final List<Date> SPECIAL_EVENT_STARRED_DATES = List.of(Date.valueOf(3),
         Date.valueOf(10), Date.valueOf(17), Date.valueOf(24), Date.valueOf(25), Date.valueOf(31));
-    public static final int SPECIAL_EVENT_DISCOUNT_AMOUNT = 1000;
+    public static final int SPECIAL_EVENT_DISCOUNT_AMOUNT = 1_000;
 
+    public static final int PRESENT_EVENT_PRICE_CONDITION = 120_000;
+    public static final Menu PRESENT_EVENT_MENU = CHAMPAGNE;
+    public static final int PRESENT_AMOUNT = 1;
+    public static final int PRESENT_AMOUNT_NOTHING = 0;
 
     private Constant() {
     }
