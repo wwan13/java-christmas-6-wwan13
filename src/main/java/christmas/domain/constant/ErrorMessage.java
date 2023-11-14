e8c452d4d13102901170ff0bf1abf682e529cd31
wwan13 <wwan13@naver.com>
2023-11-14 23:28:00 +0900

    refactor : 에러 메세지 `ErrorMessage`로 이동

--- src/main/java/christmas/domain/constant/ErrorMessage.java
+++ src/main/java/christmas/domain/constant/ErrorMessage.java
@@ -4,6 +4,7 @@ public class ErrorMessage {
 
     public static final String INVALID_DATE = "유효하지 않은 날짜입니다. 다시 입력해 주세요.";
     public static final String INVALID_ORDER = "유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    public static final String CANNOT_APPLY_FORMAT = "12월 %d일에는 %d 이벤트를 적용할 수 없습니다.";
 
     private ErrorMessage() {
     }
