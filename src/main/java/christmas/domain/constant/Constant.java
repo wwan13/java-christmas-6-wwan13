f931dda0e92f4c1a3eb6d2f0bb35f37afe646e08
wwan13 <wwan13@naver.com>
2023-11-14 23:27:17 +0900

    refactor : 모든 상수 `Constant`로 이동

--- src/main/java/christmas/domain/constant/Constant.java
+++ src/main/java/christmas/domain/constant/Constant.java
@@ -1,5 +1,6 @@
 package christmas.domain.constant;
 
+import christmas.domain.date.Date;
 import java.util.List;
 
 public class Constant {
@@ -14,6 +15,18 @@ public class Constant {
     public static final int SIZE_OF_WEEK = 7;
     public static final List<Integer> REMAINDERS_OF_WEEKDAYS = List.of(3, 4, 5, 6, 0);
     public static final List<Integer> REMAINDERS_OF_WEEKENDS = List.of(1, 2);
+
+    public static final Date CHRISTMAS_DAY_EVENT_START_DATE = Date.valueOf(1);
+    public static final Date CHRISTMAS_DAY_EVENT_END_DATE = Date.valueOf(25);
+    public static final int CHRISTMAS_DAY_EVENT_DEFAULT_DISCOUNT_AMOUNT = 1000;
+    public static final int CHRISTMAS_DAY_EVENT_ADDITIONAL_DISCOUNT_AMOUNT = 100;
+
+    public static final int WEEK_EVENT_DISCOUNT_AMOUNT = 2023;
+
+    public static final List<Date> SPECIAL_EVENT_STARRED_DATES = List.of(Date.valueOf(3),
+        Date.valueOf(10), Date.valueOf(17), Date.valueOf(24), Date.valueOf(25), Date.valueOf(31));
+    public static final int SPECIAL_EVENT_DISCOUNT_AMOUNT = 1000;
+
 
     private Constant() {
     }
