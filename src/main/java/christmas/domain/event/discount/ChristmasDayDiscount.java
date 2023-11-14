121fceb5cdd9a1511babb33af3a22acbb13c3552
wwan13 <wwan13@naver.com>
2023-11-14 15:40:03 +0900

    feat : `ChristmasDayDiscount` 검증 추가

--- src/main/java/christmas/domain/event/discount/ChristmasDayDiscount.java
+++ src/main/java/christmas/domain/event/discount/ChristmasDayDiscount.java
@@ -9,6 +9,8 @@ public class ChristmasDayDiscount implements Discount {
     private static final Date EVENT_END_DATE = Date.valueOf(25);
     private static final int DEFAULT_DISCOUNT_AMOUNT = 1000;
     private static final int ADDITIONAL_DISCOUNT_AMOUNT = 100;
+    private static final String ERROR_FORMAT_INVALID_DATE =
+        "%d일에는 크리스마스 디데이 할인을 적용할 수 없습니다.";
 
     @Override
     public boolean isApplicable(Date date) {
@@ -19,5 +21,13 @@ public class ChristmasDayDiscount implements Discount {
     public int offer(Date date, Order order) {
         int dateGap = date.compareTo(EVENT_START_DATE);
         return DEFAULT_DISCOUNT_AMOUNT + (dateGap * ADDITIONAL_DISCOUNT_AMOUNT);
+    }
+
+    private void validate(Date date) {
+        if (!isApplicable(date)) {
+            throw new IllegalArgumentException(
+                String.format(ERROR_FORMAT_INVALID_DATE, date.getValue())
+            );
+        }
     }
 }
