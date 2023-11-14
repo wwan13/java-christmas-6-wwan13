52918fa9d1c2b1f6b3b50b3da0d891997b7f7540
wwan13 <wwan13@naver.com>
2023-11-14 15:36:06 +0900

    feat : `Date` equals, hashcode 추가 구현

--- src/main/java/christmas/domain/date/Date.java
+++ src/main/java/christmas/domain/date/Date.java
@@ -4,6 +4,8 @@ import static christmas.domain.constant.Constant.DECEMBER_END_DATE;
 import static christmas.domain.constant.Constant.DECEMBER_START_DATE;
 import static christmas.domain.constant.ErrorMessage.INVALID_DATE;
 
+import java.util.Objects;
+
 public class Date implements Comparable<Date> {
 
     private final int value;
@@ -29,6 +31,28 @@ public class Date implements Comparable<Date> {
 
     protected int calculateRemainder(int value) {
         return this.value % value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (!(object instanceof Date)) {
+            return false;
+        }
+
+        Date date = (Date) object;
+        return value == date.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 
     @Override
