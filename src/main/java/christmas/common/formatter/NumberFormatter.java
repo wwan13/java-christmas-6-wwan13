3a7f58eb174f33528e11d139817bc269e108b6a7
wwan13 <wwan13@naver.com>
2023-11-15 23:05:37 +0900

    refactor : 숫자 포멧 상수로 변경

--- src/main/java/christmas/common/formatter/NumberFormatter.java
+++ src/main/java/christmas/common/formatter/NumberFormatter.java
@@ -4,11 +4,13 @@ import java.text.DecimalFormat;
 
 public class NumberFormatter {
 
+    private static final String NUMBER_FORMAT = "###,###";
+
     private NumberFormatter() {
     }
 
     public static String format(int value) {
-        DecimalFormat formatter = new DecimalFormat("###,###");
+        DecimalFormat formatter = new DecimalFormat(NUMBER_FORMAT);
         return formatter.format(value);
     }
 }
