f015dfb546c2f14443abf8985e5471060342f405
wwan13 <wwan13@naver.com>
2023-11-15 23:03:48 +0900

    refactor : 기본 생성자 private로 변경

--- src/main/java/christmas/common/converter/StringConverter.java
+++ src/main/java/christmas/common/converter/StringConverter.java
@@ -4,6 +4,9 @@ import java.util.function.Supplier;
 
 public class StringConverter {
 
+    private StringConverter() {
+    }
+
     public static int toIntWithExceptionThrow(String value,
         Supplier<RuntimeException> exception) throws RuntimeException {
 
